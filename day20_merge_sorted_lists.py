93292cd9c5b029e8131d8bc59d164e0654235155
kakarlayathin <kakarlayathin@gmail.com>
2026-08-13 11:12:01 +0530

    Day 20: Merge Two Sorted Lists

---
+++ day20_merge_sorted_lists.py
@@ -0,0 +1,23 @@
+print("===merge_sorted_lists===")
+def merge_lists(list1 , list2):
+    result = []
+    i = 0
+    j = 0
+    while i <len(list1) and j< len(list2):
+        if list1[i] <= list2[j]:
+            result.append(list1[i])
+            i = i +1
+        else:
+            result.append(list2[j])
+            j = j+1
+    while i < len(list1):
+        result.append(list1[i])
+        i = i+1
+    while j < len(list2):
+        result.append(list2[j])
+        j = j+1
+    return result
+
+print(merge_lists([1, 2, 4], [1, 3, 4]))  # Expected: [1, 1, 2, 3, 4, 4]
+print(merge_lists([], [0]))               # Expected: [0]
+print(merge_lists([1, 5], [2, 3, 6]))     # Expected: [1, 2, 3, 5, 6]
